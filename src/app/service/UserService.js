cac8f4f51c24e6d51c54510a9efc813907e4e995
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-17 21:47:21 -0300

    feat: add try-catch statements to the create method of the UserController class

--- src/app/service/UserService.js
+++ src/app/service/UserService.js
@@ -2,12 +2,8 @@ const UserRepository = require('../repository/UserRepository');
 
 class UserService {
   async create(payload) {
-    try {
-      const result = await UserRepository.create(payload);
-      return result;
-    } catch (error) {
-      return error;
-    }
+    const result = await UserRepository.create(payload);
+    return result;
   }
 }
 
